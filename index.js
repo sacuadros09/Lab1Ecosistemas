8978cb61fec20d7ab59eb02148da3efb79fb0205
sacuadros09 <sacuma09@gmail.com>
2024-02-11 11:17:26 -0500

    bulbasur

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 let pokeData
 
-async function getApi(pokeid) {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/${pokeid}")
+async function getApi(pokeData) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeData}`)
     const data = await response.json()
     Pokemons(data)
 }
 
+const bulbasurBtn = document.createElement("button")
+bulbasurBtn.id = "pokeBtn"
+bulbasurBtn.addEventListener("click", () => {
+    Container.innerHTML = "";
+    pokeData = 1
+    getApi(pokeData)
+})
+
+const bulbasurImg = document.createElement("img")
+bulbasurImg.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif"
+bulbasurBtn.appendChild(bulbasurImg)
+
+
+const section = document.createElement("section");
+section.appendChild(bulbasurBtn);
+
+
+document.getElementById("btnsContainer").appendChild(section);
+
+
+
+const Container = document.createElement("div");
+
+
+    Container.appendChild(image)
+    Container.appendChild(section)
